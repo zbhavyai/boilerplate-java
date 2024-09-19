ebc064f313193762ac44e790ce33e3faad5588e2
Bhavyai Gupta <zbhavyai@gmail.com>
2024-09-18 20:24:01 -0600

    fix test cases

---
+++ src/test/java/io/github/zbhavyai/rest/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.github.zbhavyai.rest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingRestTest {
+
+}
---
+++ src/test/java/io/github/zbhavyai/rest/GreetingRestTest.java
@@ -0,0 +1,30 @@
+package io.github.zbhavyai.rest;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+
+@QuarkusTest
+public class GreetingRestTest {
+
+    @Test
+    void testGreet() {
+        given()
+                .when().get("/api/v1/hello")
+                .then()
+                .statusCode(200)
+                .contentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    void testError() {
+        given()
+                .when().get("/api/v1/error")
+                .then()
+                .statusCode(400)
+                .contentType(MediaType.APPLICATION_JSON);
+    }
+}
